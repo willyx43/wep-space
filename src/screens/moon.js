e44140dd3a9c6f280c0ec1af127a5634042cec4c
willyx43 <58426827+willyx43@users.noreply.github.com>
2022-01-11 20:22:54 +0100

    add refresh + api call moon (#1)

--- src/screens/moon.js
+++ src/screens/moon.js
@@ -1,13 +1,63 @@
 import React from 'react';
 import {
-    Text,
-    View,
-  } from 'react-native';
+  Image,
+  RefreshControl,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  tinyLogo: {
+    width: 250,
+    height: 250,
+  },
+});
 
 export default function MoonScreen() {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Moon</Text>
-      </View>
-    );
+
+  const [refreshing, setRefreshing] = React.useState(false);
+  const [moon, setMoon] = React.useState(0);
+  // const onRefresh = React.useCallback(() => {
+  //   setRefreshing(true);
+  //   wait(2000).then(() => setRefreshing(false));
+  // }, []);
+
+  // React.useEffect(() => {
+    async function fetchFunction() {
+      try{
+      const response = await fetch('https://api.le-systeme-solaire.net/rest/bodies/lune');
+      const json = await response.json();
+      setMoon(json);
+      }
+      catch(err) {
+        throw err;
+      }
+    }
+    // fetchFunction();
+  // }, []);
+ 
+  return (
+    <ScrollView
+      refreshControl={
+        <RefreshControl
+          refreshing={refreshing}
+          onRefresh={() => fetchFunction()}
+        />
+      }
+      >
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Image
+        style={styles.tinyLogo}
+        source={require("../assets/moon.jpeg")}
+      ></Image>
+      <Text>name : {moon.name}</Text>
+      <Text>gravity : {moon.gravity}</Text>
+      <Text>Mean Radius {moon.meanRadius}</Text>
+      <Text>Equatorial radius in kilometres. : {moon.equaRadius}</Text>
+      <Text>Polar radius in kilometres. : {moon.polarRadius}</Text>
+    </View>
+    </ScrollView>
+  );
 }
