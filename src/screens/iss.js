5f314df21e94eae2bd88958738139db2e9dcc75f
willyx43 <willy.eparvier@epitech.eu>
2022-01-14 21:12:23 +0100

    add missions

--- src/screens/iss.js
+++ src/screens/iss.js
@@ -8,7 +8,7 @@ import {
     Text,
     View,
 } from 'react-native';
-import { Icon } from 'react-native-elements';
+import { Card, Icon } from 'react-native-elements';
 import { useNavigation } from '@react-navigation/native';
 
 
@@ -35,6 +35,9 @@ const styles = StyleSheet.create({
       left:"1%",
       zIndex:10
     },
+  footerList: {
+    marginTop: 20
+  }
 });
 
 export default function IssIno() {
@@ -100,10 +103,65 @@ export default function IssIno() {
             }
             ListFooterComponent={
               <>
-              <View style={styles.footerList}>
-                  <Text>L'ISS a une vitesse de 28,000 km/h</Text>
-                  <Text>Il y a une distance de 408 km entre la Terre et l'ISS</Text>
-              </View>
+                <View style={styles.footerList}>
+                    <Text style={{fontSize: 15}} >L'ISS a une vitesse de 28,000 km/h</Text>
+                    <Text style={{fontSize: 15}} >Il y a une distance de 408 km entre la Terre et l'ISS</Text>
+                </View>
+                <View style={{alignItems: 'center'}}>
+                  <Text style={{fontSize: 30}} >Missions :</Text>
+                </View>
+                <Card style={{alignItems: 'center'}}>
+                    <Card.Title>Soyouz MS-18</Card.Title>
+                    <Card.Image
+                      style={{ padding: 0 }}
+                      source={{
+                        uri:
+                          'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Expedition_64_Crew_Qualification_Exams_%28NHQ202009220006%29.jpg/300px-Expedition_64_Crew_Qualification_Exams_%28NHQ202009220006%29.jpg',
+                      }}
+                    />
+                    <Text h4>
+                    9 avril 2021
+                    </Text>
+                </Card>
+                <Card style={{alignItems: 'center'}}>
+                    <Card.Title>SpaceX Crew-2</Card.Title>
+                    <Card.Image
+                      style={{ padding: 0 }}
+                      source={{
+                        uri:
+                          'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/SpaceX_Crew-2_crew.jpg/300px-SpaceX_Crew-2_crew.jpg',
+                      }}
+                    />
+                    <Text h4>
+                    23 avril 2021
+                    </Text>
+                </Card>
+                <Card style={{alignItems: 'center'}}>
+                    <Card.Title>Soyouz MS-19</Card.Title>
+                    <Card.Image
+                      style={{ padding: 0 }}
+                      source={{
+                        uri:
+                          'https://kosmosnews.fr/wp-content/uploads/2021/09/SMS-19-Equipage-principal-photo-officielle_01.jpg',
+                      }}
+                    />
+                    <Text h4>
+                    5 octobre 2021
+                    </Text>
+                </Card>
+                <Card style={{alignItems: 'center'}}>
+                    <Card.Title>SpaceX Crew-3</Card.Title>
+                    <Card.Image
+                      style={{ padding: 0 }}
+                      source={{
+                        uri:
+                          'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/SpaceX_Crew-3_%28official_portrait%29.jpg/300px-SpaceX_Crew-3_%28official_portrait%29.jpg',
+                      }}
+                    />
+                    <Text h4>
+                    11 novembre 2021
+                    </Text>
+                </Card>
               </>
             }
             refreshControl={
